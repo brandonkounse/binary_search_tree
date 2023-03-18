7af0a9492250b4dea8de2b7641b3bb216fdec5bc
Brandon <kounse@gmail.com>
2023-03-18 15:27:24 -0500

    Added initialize method.

--- node.rb
+++ node.rb
@@ -3,4 +3,10 @@
 # class to represent data and next nodes
 class Node
   attr_reader :data, :left_child, :right_child
+
+  def initialize(data, left_child, right_child)
+    @data = data
+    @left_child = left_child
+    @right_child = right_child
+  end
 end
