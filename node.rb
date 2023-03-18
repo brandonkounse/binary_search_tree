09933477cd13ca965acf6762e757a735fa8dc568
Brandon <kounse@gmail.com>
2023-03-18 17:16:32 -0500

    Refactored node class to set default values to nil for left and right nodes, and only accept data as the argument.

--- node.rb
+++ node.rb
@@ -2,11 +2,11 @@
 
 # class to represent data and next nodes
 class Node
-  attr_reader :data, :left_child, :right_child
+  attr_reader :data, :left, :right
 
-  def initialize(data, left_child, right_child)
+  def initialize(data)
     @data = data
-    @left_child = left_child
-    @right_child = right_child
+    @left = nil
+    @right = nil
   end
 end
