b49885688b813aa83c4c62a30e0d3ad1449368b4
Brandon <kounse@gmail.com>
2023-03-18 21:10:21 -0500

    Finished logic for build_tree method.

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,12 +5,20 @@ class Tree
   attr_reader :root, :data
 
   def initialize(array)
-    @data = array
+    @data = array.sort.uniq
   end
 
-  def build_tree
-    sorted_data = @data.uniq.sort
-    middle = sorted_data.length / 2
-    node = sorted_data[middle]
+  def build_tree(array)
+    if array.empty?
+      nil
+    else
+      mid = array.length / 2
+
+      node = Node.new(array[mid])
+      node.left = build_tree(array[(0...mid)])
+      node.right = build_tree(array[(mid + 1..array.length - 1)])
+
+      @root = node
+    end
   end
 end
