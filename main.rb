643a2839852ac034405d6f7257aefe4cdc813b79
Brandon <kounse@gmail.com>
2023-04-01 14:44:08 -0500

    Added rebalance method call for testing.

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.build_tree(tree.data)
 tree.insert(40)
 tree.insert(319)
+tree.insert(321)
 tree.delete(8)
 tree.find(9)
 tree.level_order # { |element| puts element.data }
@@ -15,5 +16,6 @@ tree.preorder # { |element| puts element.data }
 tree.postorder # { |element| puts element.data }
 tree.height(9)
 tree.depth(4)
-p tree.balanced?
+tree.balanced?
+tree.rebalance
 tree.pretty_print
