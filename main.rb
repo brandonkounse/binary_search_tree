10fab6c0cbbc233a02260e96d30abf55483b8099
Brandon <kounse@gmail.com>
2023-04-01 12:36:15 -0500

    Added method call for depth method, passed tests.

--- main.rb
+++ main.rb
@@ -6,11 +6,13 @@ require './lib/tree'
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.build_tree(tree.data)
 tree.insert(40)
+tree.insert(319)
 tree.delete(8)
 tree.find(9)
 tree.level_order # { |element| puts element.data }
 tree.inorder # { |element| puts element.data }
 tree.preorder # { |element| puts element.data }
 tree.postorder # { |element| puts element.data }
-p tree.height(4)
+tree.height(4)
+tree.depth(4)
 tree.pretty_print
